34800a45e4e44f9a822c9941850bf667c69a0179
ArunRaj2312 <arunvijay1043534@gmail.com>
2023-01-26 21:20:40 -1200

    add file

---
+++ main.js
@@ -0,0 +1,21 @@
+var i=prompt("Enter the character");
+
+if(i>="0" && i<="9"){
+	console.log("number");
+}
+else if(i>="a" && i<="z" || i>="A" && i<="Z"){
+	console.log("alphabet");
+}
+else{
+	console.log("special characters")
+}
+/*
+1-Write A Program To Accept Three Numbers From User And Print Them In Ascending And Descending Order
+2-A school has following rules for grading system:
+a. Below 25 - F
+b. 25 to 45 - E
+c. 45 to 50 - D
+d. 50 to 60 - C
+e. 60 to 80 - B
+f. Above 80 - A
+*/
